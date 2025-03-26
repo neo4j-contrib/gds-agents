f3b28fb6c1f58b31adaee84d14823b0a74dbaa33
Jon Besga <jon.besga@neo4j.com>
2025-03-26 19:56:40 +0000

    Remove credentials and use env vars

--- mcp_server/src/mcp_server_neo4j_gds/__init__.py
+++ mcp_server/src/mcp_server_neo4j_gds/__init__.py
@@ -1,18 +1,20 @@
 from . import server
 import asyncio
 import argparse
+import os
 
 
 def main():
     """Main entry point for the package."""
     parser = argparse.ArgumentParser(description='Neo4j GDS MCP Server')
     parser.add_argument('--db-url', 
-                       default="neo4j+s://57327d08.databases.neo4j.io",
+                       default=os.environ.get("NEO4J_URI"),
                        help='URL to Neo4j database')
     parser.add_argument('--username', 
-                       default="neo4j",
+                       default=os.environ.get("NEO4J_USERNAME", "neo4j"),
                        help='Username for Neo4j database')
     parser.add_argument('--password', 
+                       default=os.environ.get("NEO4J_PASSWORD"),
                        help='Password for Neo4j database')
     
     args = parser.parse_args()
